8520f6aa7d332a3c7f79f5dfaa2e3d70e8987c5c
aryan26aa <shivaverma7777@gmail.com>
2026-04-02 11:44:55 +0530

    dummmy apis

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const cookieParser=require("cookie-parser");
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
+//dummy api
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Welcome to bank system API"});
+});
 //require routes
 const AuthRouter=require("./routes/auth.route");
 const AccountRouter=require("./routes/account.router");
